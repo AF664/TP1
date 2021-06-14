5e82c2421a29e3bcf91c3c6fb701e51f6f5b5c90
AF664 <alexisfuhr7@gmail.com>
2021-06-14 16:23:56 -0300

    se agrego la lectura de multiplicacion por linea de comando. El valor se guardo en la variable global multiplicacion.

--- main.cpp
+++ main.cpp
@@ -13,11 +13,14 @@
 // en ninguna clase
 #include "utils.h" 
 
+#define CLASSIC 0
+#define KARATSUBA 1
+
 #define MSJ_AYUDA "USO calculadora.exe -i <input file> -o <output file> -p <precision>\
 \n<input file>  nombre del archivo de entrada. Stdin por defecto\
 \n<output file> nombre del archivo de salida. Stdout por defecto\
-\n<precision>   precision de los números a utilizar \n" 
-
+\n<precision>   precision de los números a utilizar \
+\n<multiplicacion>   Algoritmo de multiplicacion a utilizar. classic o karatsuba \n" 
 using namespace std;
 
 
@@ -32,18 +35,21 @@ static void imprimir_ayuda(string const &arg);
 
 static void opt_precision(string const &precision);
 
+static void opt_multiplication(string const &mul);
 
 option_t config[] = {
     {1, "i", "input","-", opt_input, OPT_DEFAULT},
     {1, "o", "output","-", opt_output, OPT_DEFAULT},
     {1, "p", "precision",PRECISION_DEFAULT,opt_precision, OPT_DEFAULT},
-    {0, "h", "help",nullptr,imprimir_ayuda, OPT_DEFAULT},
+    {1, "m", "multiplicacion", MULTIPLICATION_DEFAULT, opt_multiplication, OPT_DEFAULT},
+	{0, "h", "help",nullptr,imprimir_ayuda, OPT_DEFAULT},
     {0,},// Fin de opciones
 
 };
 
 
 static int precision;
+static int multiplication;
 static istream *iss;
 static ostream *oss;
 static fstream ifs;
@@ -142,6 +148,35 @@ static void opt_precision(string const &arg)
 		     << endl;
 		exit(1);
 	}
+}
+
+static void opt_multiplication(string const &arg)
+{
+	istringstream iss(arg);
+	string word;
+	int i = 0;
+	// Intentamos extraer el factor de la línea de comandos.
+	// Para detectar argumentos tipo char que conforman una
+	// palabra, vamos a verificar que EOF llegue justo
+	// después de la lectura exitosa del escalar.
+	//
+
+	if (!(iss >> word)
+	    || !iss.eof()) {
+		cerr << "non-integer factor: "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+
+	if (iss.bad()) {
+		cerr << "cannot read integer factor."
+		     << endl;
+		exit(1);
+	}
+	if (word == "classic" || word == "standard") multiplication = CLASSIC;
+	else multiplication = KARATSUBA;
 }
 
 static void imprimir_ayuda(string const &arg)
